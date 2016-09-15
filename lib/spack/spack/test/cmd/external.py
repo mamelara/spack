bc273db0075646fa6cd0348804e6d5dbe3deef84
Mario Melara <maamelara@gmail.com>
2016-09-15 14:47:55 -0700

    Add tests to detect different formats of paths
    Tests whether spack external -p [path] can correctly parse version
    strings from the list of directory names.

--- lib/spack/spack/test/cmd/external.py
+++ lib/spack/spack/test/cmd/external.py
@@ -1,10 +1,8 @@
-import re
+import argparse
 from spack.test.mock_packages_test import *
-from spack.build_environment import create_module_cmd
 from spack.spec import Spec
-from spack.util import spack_yaml 
 import spack.config
-import spack.cmd.external
+import spack.cmd.external as external
 
 class MockArgs(object):
     def __init__(self, package_name, path, module, cspec=[]):
@@ -17,14 +15,41 @@ class ExternalCmdTest(MockPackagesTest):
     """ Test the external creation of a external spec and also test
     that it gets properly written to a packages.yaml """
 
+    def test_external_command_operations(self):
+        parser = argparse.ArgumentParser()
+        external.setup_parser(parser)
+        args1 = parser.parse_args(['-m', 'cray-hdf5', '%gcc', '%clang'])
+        args2 = parser.parse_args(['-p', '/path/to/hdf5', '%intel', '%gcc']) 
+        self.assertRaises(ValueError, external.external, parser, args1)
+        self.assertRaises(ValueError, external.external, parser, args2)
+
+    def test_detect_various_types_of_paths(self):
+        """Given a path correctly detect the version from path.
+           Tests different formats
+        opt_path_package1 is opt/openmpi-1.4.3
+        opt_path_package2 is opt/openmpi/1.4.3
+        opt_path_package3 is opt/openmpi-1.4.3-6asg2easdar3 (mimics spack)
+        """
+        
+        mock_args1 = MockArgs("openmpi", self.opt_path_package1, "", ["%gcc", "%clang"])
+        mock_args2 = MockArgs("openmpi", self.opt_path_package2, "", ["%gcc", "%clang"])
+        mock_args3 = MockArgs("openmpi", self.opt_path_package3, "", ["%gcc", "%clang"])
+        ver1 = external.get_versions_from_args(mock_args1)
+        ver2 = external.get_versions_from_args(mock_args2)
+        ver3 = external.get_versions_from_args(mock_args3)
+        print ver1
+        assert(ver1[1] != [])
+        assert(ver2[1] != [])
+        assert(ver3[1] != [])
+        self.assertTrue(ver1 == ver2 and ver2 == ver3)
+
     def test_grep_external_from_module_name(self):
         """Given a module arg create a list of versions and package name"""
         mock_args = MockArgs("hdf5", "", "cray-hdf5", ["%gcc", "%clang"])
         if spack.architecture.sys_type() == "cray":
             matches = ['cray-hdf5', ['1.10.0','1.8.14','1.8.16']]
             self.assertEqual(matches, 
-                            spack.cmd.external.get_versions_from_args(
-                                                                    mock_args))
+                             external.get_versions_from_args(mock_args))
         else:
             return True
 
@@ -35,7 +60,7 @@ class ExternalCmdTest(MockPackagesTest):
         # Need to have fake paths for these packages for the test to work.
         matches = ["hdf5", ["2.0.0", "1.8.16", "1.9.2"]]
         self.assertEqual(matches, 
-                         spack.cmd.external.get_versions_from_args(mock_args))
+                         external.get_versions_from_args(mock_args))
 
 
     def test_yaml_dict_creation(self):
@@ -45,10 +70,7 @@ class ExternalCmdTest(MockPackagesTest):
         test_spec_clang = "hdf5@1.8.1%clang"
         package_specs[test_spec_gcc] = "cray-hdf5"
         package_specs[test_spec_clang] = "cray-hdf5"
-
-        specs = spack.cmd.external._create_yaml_dict("hdf5", "1.8.1", cspec,
-                                                     "cray-hdf5") 
-        
+        specs = external._create_yaml_dict("hdf5", "1.8.1", cspec, "cray-hdf5")  
         self.assertEqual(package_specs.keys(), specs.keys())
 
 
@@ -59,12 +81,11 @@ class ExternalCmdTest(MockPackagesTest):
         old_yaml_dict = spack.config.get_config("packages") 
         cspec = ["%gcc", "%clang"]
 
-        yaml_entry = spack.cmd.external._create_yaml_dict("hdf5",
-                                                          "1.8.1", cspec,
-                                                          "cray-hdf5") 
+        yaml_entry = external._create_yaml_dict("hdf5", "1.8.1", cspec, 
+                                                                 "cray-hdf5") 
 
-        yaml_entry = spack.cmd.external._create_json_entry("hdf5", yaml_entry, 
-                                                           "modules")
+        yaml_entry = external._create_json_entry("hdf5", yaml_entry, 
+                                                         "modules")
 
         spack.config.update_config("packages", yaml_entry) 
 
