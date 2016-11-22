bcd67618bad7e52bebfb0530aa9377df4fa1d992
Mario Melara <maamelara@gmail.com>
2016-11-21 16:48:34 -0800

    Improve readability of tests.
    Hopefully improves the readability of the external cmd tests. Created
    some functions to construct packages used for specific tests.

--- lib/spack/spack/test/cmd/external.py
+++ lib/spack/spack/test/cmd/external.py
@@ -1,112 +1,140 @@
-import argparse
 import spack.architecture
 from spack.test.mock_packages_test import *
-from spack.spec import Spec
-from llnl.util.filesystem import mkdirp, join_path, touchp, touch
+import spack.cmd.external
 import spack.config
-import spack.cmd
-import spack.cmd.external as external
-from spack.external_package import ExternalPackage
+import spack.spec
+from spack.external_package import *
 
 class MockArgs(object):
-    """ Mock arguments that could be passed via the command line """
-    def __init__(self, **kwargs): # Use kwargs to generalize for both add & rm
-        attrs = ["package_spec", "external_location"]
-        for attr in attrs:
-            setattr(self, attr, None)
-
-        for k, v in kwargs.iteritems():
-            setattr(self, k, v)
-
-        self.scope = "site" # Hard code to use 'user' configs
+    def __init__(self, package_spec="", external_location="unknown"):
+        self.package_spec = package_spec
+        self.external_location = external_location
+        self.scope = "site"  # Hardcoded for consistency in using site scope
 
 
-def make_fake_install_path(path, exe_name):
-    temp_path = tempfile.mkdtemp()
-    fake_root_path = join_path(temp_path, path)
-    exe_path = join_path(fake_root_path, "bin", exe_name)
-    touchp(exe_path)
-    for p in ["lib", "share", "include"]:
-        package_path = join_path(fake_root_path, p)
-        mkdirp(package_path)
-    return fake_root_path 
-        
+def setup_external_package(package, external_type):
+    spec = spack.spec.Spec(package)
+    if external_type == "paths":
+        external_location = "path/to/%s" % spec.name
+    else:
+        external_location = spec.name
+    return ExternalPackage(spec, False, external_type, external_location)
 
-def remove_install_path(path):
-    shutil.rmtree(path, ignore_errors=True)
+
+def get_packages_config_file():
+    return spack.config.get_config("packages", "site")
+
+
+def get_specs_section(packages_config, name_of_package, external_type):
+    return packages_config.get(name_of_package, {}).get(external_type, {})
+
+
+# Create different types of packages
+def create_duplicate_package():
+    return ExternalPackage("externaltool@1.0%gcc@4.5.0",
+                           False,
+                           "paths",
+                           "path/to/external_tool")
+
+
+def create_new_external_package_to_add():
+    return ExternalPackage("externallibrary@1.9.5%gcc@6.1.0",
+                           False,
+                           "paths",
+                           "path/to/externallibrary")
+
+
+def create_external_package_to_add_to_existing_package():
+    return ExternalPackage("externaltool@1.5%gcc@4.5.0",
+                           False,
+                           "paths",
+                           "path/to/externaltool_ver1.5")
 
 
 class ExternalCmdTest(MockPackagesTest):
     """ Test the external creation of a external spec and also test
-    that it gets properly written to a packages.yaml """
+    that it gets properly written to a packages.yaml
+    """
+    #########################
+    # test spack external add
+    #########################
 
-    def test_user_path_input_appends_to_packages_config(self):
-        path = "opt/external_package/1.8.5"
-        external_package_path = make_fake_install_path(path, "externalpackage")
-        args = MockArgs(package_spec="externalpackage@1.8.5%gcc@4.3",
-                        external_location=external_package_path)
+    def test_append_new_entry_to_config(self):
+        external_library = create_new_external_package_to_add()
+        add_external_package(external_library, "site")
+        full_packages_config = get_packages_config_file()
+        self.assertTrue("externallibrary" in full_packages_config.keys())
 
-        old_packages_config = spack.config.get_config("packages", 
-                                                      scope=args.scope)
-        external.external_add(args) 
-        updated_packages_config = spack.config.get_config("packages",
-                                                          scope=args.scope)
-        remove_install_path(path)
+        # Check whether the added entry has an appropriate structure
+        specs_section = get_specs_section(full_packages_config,
+                                          external_library.name,
+                                          "paths")
+        self.assertTrue("externallibrary@1.9.5%gcc@6.1.0" in
+                        specs_section.keys())
+        self.assertTrue("path/to/externallibrary" in specs_section.values())
 
-        self.assertTrue("externalpackage" in updated_packages_config)
-        externalpackage = updated_packages_config["externalpackage"]
-        self.assertTrue(externalpackage["paths"] == 
-                {"externalpackage@1.8.5%gcc@4.3": external_package_path})
+    def test_append_to_existing_entry(self):
+        external_tool = create_external_package_to_add_to_existing_package()
+        add_external_package(external_tool, "site")
+        full_packages_config = get_packages_config_file()
+        self.assertTrue(1 == full_packages_config.keys().count("externaltool"))
 
-    def test_avoid_duplicate_entries_to_package_yaml(self):
+        specs_section = get_specs_section(full_packages_config,
+                                          external_tool.name,
+                                          "paths")
+        self.assertTrue(str(external_tool.spec) in specs_section.keys())
+        # Test that we didn't overwrite the entire entry with our new one
+        self.assertTrue(len(specs_section.keys()) > 1)
+
+    def test_avoid_duplicate_to_existing_entry(self):
+        external_tool = create_duplicate_package()
+        add_external_package(external_tool, "site")
+        full_packages_config = get_packages_config_file()
+        externaltool_specs = get_specs_section(full_packages_config,
+                                               external_tool.name,
+                                               "paths")
+
+        self.assertTrue(externaltool_specs.keys().count(
+            "externaltool@1.0%gcc@4.5.0") == 1)
+
+    def test_external_cmd_add(self):
+        """
+        Tests full function path from user arguments to addition of entries to
+        packages.yaml
+        """
+        self.make_fake_install_path("external_package/2.0", "externalpackage")
+        args = MockArgs("externalpackage@2.0%gcc@4.5.0",
+                        self.external_package_path)
+        spack.cmd.external.external_add(args)
+        packages_config = get_packages_config_file()
+        self.assertTrue("externalpackage" in packages_config.keys())
+
+    ########################
+    # test spack external rm
+    ########################
+    def test_remove_correct_entry_from_config(self):
+        spec = spack.spec.Spec("externalmodule@1.0%gcc@4.5.0")
+        remove_package_from_packages_config(spec, "site")
+        full_packages_config = get_packages_config_file()
+        external_module_specs = get_specs_section(full_packages_config,
+                                                  spec.name,
+                                                  "modules")
+        self.assertTrue("externalmodule@1.0%gcc@4.5.0"
+                        not in external_module_specs.keys())
+
+    def test_error_thrown_when_spec_not_specific(self):
+        spec = spack.spec.Spec("externalmodule@1.0")
+        with self.assertRaises(PackageSpecInsufficientlySpecificError):
+            remove_package_from_packages_config(spec, "site")
+
+    def test_remove_entire_entry_after_specs_section_is_empty(self):
         spec = spack.spec.Spec("externaltool@1.0%gcc@4.5.0")
-        external_package = ExternalPackage(spec,
-                                           "/path/to/external_tool",
-                                           "paths")
+        remove_package_from_packages_config(spec, "site")
+        full_packages_config = get_packages_config_file()
+        self.assertTrue("externaltool" not in full_packages_config.keys())
+        self.assertTrue("externalvirtual" in full_packages_config.keys())
 
-        old_packages_config = spack.config.get_config("packages", scope="site")
-        external.add_to_packages_yaml(external_package, scope="site")
-        updated_packages_config = spack.config.get_config("packages", 
-                                                          scope="site")
-
-        externaltool_config = updated_packages_config["externaltool"]["paths"]
-        self.assertTrue(externaltool_config.keys().count(
-                            "externalpackage@1.0%gcc@4.5.0") < 2)
-
-    def test_appending_to_an_existing_entry(self):
-        path = "path/to/external_tool"
-        external_package = make_fake_install_path(path, "externaltool")
-        args = MockArgs(package_spec="externaltool@2.0%gcc@4.5.0",
-                        external_location=external_package)
-        old_packages_config = spack.config.get_config("packages", 
-                                                      scope=args.scope)
-        external.external_add(args)
-
-        updated_packages_config = spack.config.get_config("packages",
-                                                          scope=args.scope)
-        remove_install_path(path)
-        self.assertTrue("externaltool" in updated_packages_config)
-        externaltool = updated_packages_config["externaltool"]
-        self.assertTrue(args.package_spec in externaltool["paths"] and
-                        "externaltool@1.0%gcc@4.5.0" in externaltool["paths"])
-
-    def test_remove_entry_from_entry(self):
-       args = MockArgs(package_spec="externaltool@1.0%gcc@4.5.0")
-       package_config = spack.config.get_config("packages", scope="site")
-       external.external_rm(args)
-       updated_config = spack.config.get_config("packages", scope="site")
-       self.assertTrue("externaltool" not in updated_config.keys() and
-                        updated_config != {}) 
-
-       args = MockArgs(package_spec="externalmodule@1.0%gcc@4.5.0")
-       external.external_rm(args)
-       updated_config = spack.config.get_config("packages", scope="site")
-       externalmodule = updated_config["externalmodule"]
-       specs = externalmodule["modules"]
-
-       self.assertTrue("externalmodule@1.0%gcc@4.5.0" not in specs.keys())
-
-    def test_handles_non_existent_spec(self):
-        args = MockArgs(package_spec="externalmodule@2.0")
-        self.assertRaises(SystemExit, external.external_rm, args)
-
+    def test_remove_when_spec_is_not_found(self):
+        spec = spack.spec.Spec("boost%gcc@6.1.0")
+        with self.assertRaises(SystemExit):  # tty.die error
+            remove_package_from_packages_config(spec, "site")
