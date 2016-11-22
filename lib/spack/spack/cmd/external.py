c9f5c45b1e7947ae6605a40152ecb7c3543b4880
Mario Melara <maamelara@gmail.com>
2016-11-21 16:45:59 -0800

    Implements spack external list
    First implementation of spack external list. Will continue working to
    get the output formatted correctly

--- lib/spack/spack/cmd/external.py
+++ lib/spack/spack/cmd/external.py
@@ -22,21 +22,18 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-import argparse
-import os
-import re
 
+from llnl.util.lang import index_by
 import llnl.util.tty as tty
 from llnl.util.tty.colify import colify
+from llnl.util.tty.color import colorize
 import spack
-from spack.build_environment import create_module_cmd
 import spack.cmd
 import spack.compilers
 import spack.config
-from spack.external_package import ExternalPackage
+import spack.external_package as ext_package
 import spack.error
-from spack.util.spack_yaml import syaml_dict
-from spack.spec import Spec
+import spack.spec
 
 description = "Add an external package entry to packages.yaml"
 
@@ -45,15 +42,17 @@ def setup_parser(subparser):
     """
     Sets up parser for external add and external rm. The arguments are as
     follows:
-        spack external add [package_spec] [path or module]  
-    """      
+        spack external add [package_spec] [path or module]
+    """
     scopes = spack.config.config_scopes
-    # Set up subcommands external and rm
-    sp = subparser.add_subparsers(metavar="SUBCOMMAND", 
+    # Set up subcommands external and rm and list
+    sp = subparser.add_subparsers(metavar="SUBCOMMAND",
                                   dest = "external_command")
+    ################
     # external add
-    add_parser = sp.add_parser(
-            'add', help="Add packages to Spack's config file")
+    ################
+    add_parser = sp.add_parser('add',
+                               help="Add packages to Spack's config file")
     add_parser.add_argument("package_spec",
                             help="spec for external package")
     add_parser.add_argument("external_location",
@@ -62,6 +61,9 @@ def setup_parser(subparser):
                             default=spack.cmd.default_modify_scope,
                             help="Configuration scope to modify.")
 
+    ###############
+    # external rm
+    ###############
     rm_parser = sp.add_parser('rm', help="Delete an entry from packages.yaml")
     rm_parser.add_argument("package_spec",
                            help="spec of a package to delete")
@@ -69,146 +71,48 @@ def setup_parser(subparser):
                            default=spack.cmd.default_modify_scope,
                            help="Configuration scope to modify.")
 
-
-def duplicate_specs(spec, specs_from_yaml):
-    """ Checks to see if an entry from a yaml contains the same spec
-        Returns true iff there is a spec match, else false"""
-    for k in specs_from_yaml.keys():
-        if Spec(k) == spec:
-            return True
-    return False
-
-
-def get_specs_from_package_yaml(package, ext_type):
-    """ Returns the specs dictionary of a package entry """
-    return package.get(ext_type, {})
-
-
-def output_successful_addition_to_yaml(spec):
-    """Output message to user when a package is added to the config"""
-    tty.msg("Added %s to packages.yaml" % spec)
-
-
-def add_entry_to_yaml(external_package_config, scope):
-    """Adds a completely new entry to the config file """
-    spack.config.update_config("packages", 
-                               external_package_config, 
-                               scope=scope)
-
-
-
-def update_yaml_entry(external_package, package_yaml):
-
-    def update_specs(old_specs, new_spec):
-        """ add new spec dict to the old spec dict """
-        copy_specs = old_specs.copy()
-        copy_specs.update(new_spec)
-        package_yaml[external_package.external_type] = copy_specs
-        return package_yaml
-
-    external_type = external_package.external_type
-    specs_from_yaml = get_specs_from_package_yaml(package_yaml, external_type) 
-    updated_package_yaml = update_specs(specs_from_yaml, 
-                                        external_package.spec_dict())
-
-    return {external_package.name: updated_package_yaml}
-    
-
-def append_to_existing_entry(external_package, package_yaml, scope):
-    """Appends spec to an existing package if the spec does not already
-    exist"""
-    specs_dict = get_specs_from_package_yaml(package_yaml, 
-                                             external_package.external_type)
-
-    if not duplicate_specs(external_package.spec, specs_dict):
-        updated_package_yaml = update_yaml_entry(external_package, 
-                                                 package_yaml)
-        add_entry_to_yaml(updated_package_yaml, scope=scope)
-        output_successful_addition_to_yaml(str(external_package))
-    else:
-        tty.msg("Added no new entries to packages.yaml")
-
-
-def get_external_from_spec(spec, scope):
-    package_name = spec.name
-    packages = spack.config.get_config("packages", scope=scope)
-    return packages.get(spec.name, {})
-
-
-def add_to_packages_yaml(external_package, scope):
-    """Given an external package and scope, add the external package to
-       the packages.yaml config file. """
-
-    package_entry_from_yaml = get_external_from_spec(external_package.spec,
-                                                     scope)
-    if not package_entry_from_yaml:
-        add_entry_to_yaml(external_package.to_dict(), scope=scope)
-        output_successful_addition_to_yaml(str(external_package))
-    else:
-        append_to_existing_entry(external_package,
-                                package_entry_from_yaml, 
-                                scope)
-    
+    ###############
+    # external list
+    ###############
+    ls_parser = sp.add_parser('list', help="List all available packages")
+    ls_parser.add_argument("--scope", choices=scopes,
+                           default=spack.cmd.default_modify_scope,
+                           help="Configuration scope to read from")
 
 def external_add(args):
-    """ Find packages and add them to packages.yaml"""
+    """Add an external package to packages.yaml config."""
     package_spec = spack.spec.Spec(args.package_spec)
     external_location = args.external_location
     scope = args.scope
-    external_package = ExternalPackage.detect_package(package_spec, 
-                                                      external_location)
-    add_to_packages_yaml(external_package, scope)
-
-
-def find_specs_and_external_type(package_yaml):
-    for ext_type in ["modules", "paths"]:
-        specs_section = get_specs_from_package_yaml(package_yaml, ext_type)
-        if specs_section:
-            return specs_section, ext_type
-    return {}, "unknown"
-
-
-def filter_specs(spec, specs_section):
-    return {k: v for k, v in specs_section.items() if Spec(k) != spec}
-
-
-def remove_package_entry(package_spec, scope):
-    packages = spack.config.get_config("packages", scope)
-    packages.pop(package_spec.name, None)
-    return packages
-
-
-def write_to_packages_yaml(updated_packages_yaml, scope):
-    """Overwrite entire packages yaml file with updated entries.
-       The reason for this is that dict.update does not really
-       update the config file when an entry is deleted"""
-    scope = spack.config.validate_scope(scope)
-    scope.sections["packages"] = {"packages": updated_packages_yaml}
-    scope.write_section("packages")
+    external_package = ext_package.ExternalPackage.create_external_package(
+                                            package_spec, external_location)
+    ext_package.add_external_package(external_package, scope)
 
 
 def external_rm(args):
+    """Removes an external package from packages.yaml"""
     package_spec = spack.spec.Spec(args.package_spec)
     scope = args.scope
-    package_yaml = get_external_from_spec(package_spec, scope)
-    specs_section, external_type = find_specs_and_external_type(package_yaml)
+    ext_package.remove_package_from_packages_config(package_spec, scope)
 
-    if not specs_section:
-        tty.die("Could not find package %s in packages.yaml")
 
-    filtered_specs = filter_specs(package_spec, specs_section) 
+def external_list(args):
+    tty.msg("Available external packages")
+    scope = args.scope
+    packages_yaml = ext_package.PackagesConfig(scope)
+    all_packages = packages_yaml.all_external_packages()
+    for package_name in all_packages.keys():
+        if package_name == "all":
+            continue
+        package_entry = packages_yaml.get_package(package_name)
+        tty.hline(colorize(package_entry._package_name), char='-')
+        for spec, location in package_entry.specs_section().iteritems():
+            print spec, location
 
-    if not filtered_specs:
-        updated_packages_yaml = remove_package_entry(package_spec, scope)
-        write_to_packages_yaml(updated_packages_yaml, scope)
-    else:         
-        package_yaml[external_type] = filtered_specs
-        external_package_config = {package_spec.name : package_yaml}
-        add_entry_to_yaml(external_package_config, scope)
-        tty.msg("Removed %s from packages.yaml" % package_spec)
-    
+
 
 def external(parser, args):
     action = {"add": external_add,
-              "rm" : external_rm }
+              "rm" : external_rm,
+              "list": external_list}
     action[args.external_command](args)
