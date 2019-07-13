e06b95884130ac19e713b8b1321a3f85fb3da984
Mario <maamelara@gmail.com>
2019-07-12 18:54:10 -0700

    Remove conflict and throw error building on Cray
    Cray compiler wrappers were likely causing unzip to not build on Cray
    machines, however, any package that depends on unzip will then be
    unbuildable (eg vim). By removing conflict we can at least throw an
    error if the user tries to build it for the backend but also allows the
    user to install it if they are just using the regular compilers.

--- var/spack/repos/builtin/packages/unzip/package.py
+++ var/spack/repos/builtin/packages/unzip/package.py
@@ -14,14 +14,29 @@ class Unzip(MakefilePackage):
 
     version('6.0', '62b490407489521db863b523a7f86375')
 
-    conflicts('platform=cray', msg='Unzip does not currently build on Cray')
-
     make_args = ['-f', 'unix/Makefile']
-    build_targets = make_args + ['generic']
 
     def url_for_version(self, version):
         return 'http://downloads.sourceforge.net/infozip/unzip{0}.tar.gz'.format(version.joined)
 
     @property
+    def build_targets(self):
+        if not self.spec.satisfies("platform=cray target=be"):
+            make_args.append("generic")
+            return make_args
+        else:
+            raise InstallError("""Unzip does not build for the backend
+architecture on Crays. If you want to use unzip for the backend it is recommended
+that you add unzip to packages.yaml:
+
+packages:
+   unzip:
+     buildable: false
+     paths:
+       unzip: /usr
+""")
+
+    @property
     def install_targets(self):
-        return self.make_args + ['prefix={0}'.format(self.prefix), 'install']
+        if not self.spec.satisfies("platform=cray target=be"):
+            return self.make_args + ['prefix={0}'.format(self.prefix), 'install']
